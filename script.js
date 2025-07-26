b84029ccc8ca463c3728dbb9615ae5c1b46abed0
mirayatechlabs <mirayatechlabs@gmail.com>
2025-07-26 19:29:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
-
-window.addEventListener("load", () => {
-  setTimeout(() => {
-    document.querySelector(".loader").style.display = "none";
-    document.querySelector(".content").classList.remove("hidden");
-
-    gsap.from(".main-logo", { duration: 1, y: -100, opacity: 0 });
-    gsap.from(".hero-title", { duration: 1.2, x: -200, opacity: 0 });
-    gsap.from(".hero-subtitle", { duration: 1.2, x: 200, opacity: 0 });
-    gsap.from(".cta-button", { duration: 1.2, y: 100, opacity: 0 });
-  }, 2500);
-});
+.loader-logo {
+  width: 40vw;
+  max-width: 600px;
+  animation: pulse 2s infinite;
+}
